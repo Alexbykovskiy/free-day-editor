d58ac8a7f29be974bdb7a8b79aa82148bf68ff2c
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-12-04 21:55:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ titleMonthFontSizeInput,
 
     footerFontSizeInput,
     footerFontWeightSelect,
-    footerFontStyleSelect;
+    footerFontStyleSelect,
   yearInput,
   titleMainInput,
   titleMonthInput,
